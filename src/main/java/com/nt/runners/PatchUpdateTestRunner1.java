773366dea76ad34a2b1e0b3d61b8c2701f4be951
Bhushan9309 <120237192+Bhushan9309@users.noreply.github.com>
2025-12-14 19:48:35 +0530

    Done
    Update Done

--- src/main/java/com/nt/runners/PatchUpdateTestRunner1.java
+++ src/main/java/com/nt/runners/PatchUpdateTestRunner1.java
@@ -1,35 +1,32 @@
 package com.nt.runners;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
 import org.springframework.http.HttpMethod;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-
 //@Component
-public class DeleteTouristByIdTestRunner implements CommandLineRunner {
+public class PatchUpdateTestRunner1 implements CommandLineRunner {
 
 	@Autowired
     private RestTemplate template;
 	@Override
 	public void run(String... args) throws Exception {
 		//=============using exchange()==============//
-				// endpoint url
-				String endpointurl="http://localhost:8080/BootRestProj10-MiniProject-SwaggerAPI/tourist-api/delete/{id}";
-				//consume the endpoint
-				ResponseEntity<String> resp=template.exchange(endpointurl, HttpMethod.DELETE,null,String.class,1010);
+				// end point url
+				String endpointurl="http://localhost:8080/BootRestProj10-MiniProject-SwaggerAPI/tourist-api/update/{id}/{addrs}";
+				//register factory
+				template.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+				//consume the end point
+				ResponseEntity<String> resp=template.exchange(endpointurl, HttpMethod.PATCH,null,String.class,1004,"London");
 				//display the results
 				System.out.println("Response Status Code ---> "+resp.getStatusCode());
 				System.out.println("Response Body --> "+resp.getBody());
 				System.out.println("Response Headers ==> "+resp.getHeaders());
 				System.exit(0);
-
 
 	}
 
--- src/main/java/com/nt/runners/PatchUpdateTestRunner2.java
+++ src/main/java/com/nt/runners/PatchUpdateTestRunner2.java
@@ -1,36 +1,31 @@
 package com.nt.runners;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
 import org.springframework.http.HttpMethod;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-
-//@Component
-public class DeleteTouristByIdTestRunner implements CommandLineRunner {
+@Component
+public class PatchUpdateTestRunner2 implements CommandLineRunner {
 
 	@Autowired
     private RestTemplate template;
 	@Override
 	public void run(String... args) throws Exception {
 		//=============using exchange()==============//
-				// endpoint url
-				String endpointurl="http://localhost:8080/BootRestProj10-MiniProject-SwaggerAPI/tourist-api/delete/{id}";
-				//consume the endpoint
-				ResponseEntity<String> resp=template.exchange(endpointurl, HttpMethod.DELETE,null,String.class,1010);
+				// end point url
+				String endpointurl="http://localhost:8080/BootRestProj10-MiniProject-SwaggerAPI/tourist-api/update/{loc1}/{loc2}/{loc3}/{hikePercent}";
+				//register factory
+				template.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+				//consume the end point
+				ResponseEntity<String> resp=template.exchange(endpointurl, HttpMethod.PATCH,null,String.class,"Goa","Varanasi","Nagpur",7.0);
 				//display the results
 				System.out.println("Response Status Code ---> "+resp.getStatusCode());
 				System.out.println("Response Body --> "+resp.getBody());
 				System.out.println("Response Headers ==> "+resp.getHeaders());
 				System.exit(0);
 
-
 	}
-
 }
